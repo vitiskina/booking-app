c63cb56f9bb11f0de16bca32477ef004732dea05
quoc thai <lqthai5121@gmail.com>
2023-03-14 18:06:53 +0700

    done before deploy maybe add chat function

--- client/src/pages/SearchPage.jsx
+++ client/src/pages/SearchPage.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 import React, { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import ErrNotification from "../ErrNotification";
+import PlaceImg from "../PlaceImg";
 import { UserContext } from "../UserContext";
 
 const SearchPage = () => {
@@ -27,22 +29,26 @@ const SearchPage = () => {
       info={"Fill the search bar to searching place!"}
     />
   ) : searchResults?.length > 0 ? (
-    searchResults.map((place) => (
-      <div className="flex mt-4 bg-gray-200 p-2 rounded-2xl ">
-        <div>
-          <img src="" />
-        </div>
-        <div>
-          {" "}
-          <h1>{place.title}</h1>
-        </div>
-      </div>
-    ))
+    <div to className="mt-8">
+      {searchResults.map((place) => (
+        <Link to={'/place/'+ place._id} className="lg:mx-64 flex items-center my-3 bg-gray-200 p-2 rounded-2xl ">
+          <div className=" mr-4 flex w-36 h-36 bg-gray-300 rounded-2xl shrink-0">
+            {place.photos.length > 0 && <PlaceImg place={place} />}
+          </div>
+          <div className="grow-0 shrink">
+            <h2 className="text-xl font-semibold border-b border-gray-400 my-2 pb-2">
+              {place.title}
+            </h2>
+            <p className="text-sm mt-2">{place.description}</p>
+          </div>
+        </Link>
+      ))}
+    </div>
   ) : (
     <ErrNotification
-    srcImg={
-      "https://wizzy.ai/blog/wp-content/uploads/2020/10/No-Result-Found-Accept.jpg"
-    }
+      srcImg={
+        "https://wizzy.ai/blog/wp-content/uploads/2020/10/No-Result-Found-Accept.jpg"
+      }
       info={"Can't find your place! Check the name of place again!"}
     />
   );
